433de2a7f626a31dbf14a50ddb908b89b4e72609
Topology12 <duong120503mk@gmail.com>
2024-07-20 20:35:45 +0700

    Basic-2

---
+++ Basic_2.py
@@ -0,0 +1,22 @@
+# Datatypes
+# String
+s = "Hello"
+first_character = s[0]
+last_character = s[4]
+print(first_character + last_character)
+
+# Integer
+
+num_int = 123
+print(num_int + 321)
+
+# Float
+
+Pi = 3.14
+print("PI =", Pi)
+
+# Boolean
+
+True_val = True
+False_val = False
+print(True_val, False_val)
