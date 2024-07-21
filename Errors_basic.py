52a7735e33fb7050a6acf2b7f6f884bc3db97ffc
Topology12 <duong120503mk@gmail.com>
2024-07-21 20:06:26 +0700

    TypeErrorsAndConvertDatatypes

---
+++ Errors_basic.py
@@ -0,0 +1,17 @@
+# Type Error:
+# len(integer)
+print(len(1))
+
+# str + int
+age = 21
+print("Im " + age + "years old")
+
+# Convert data types
+# Convert String to Integer
+print(int("123"))
+# Convert int to String
+print(str(123))
+# Convert float to Integer
+print(int(123.5))
+# Convert Integer to Float
+print(float(123))
