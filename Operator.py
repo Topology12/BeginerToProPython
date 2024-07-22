bf9c7da3454895f83d406cf8c98fe56cb72db099
Topology12 <duong120503mk@gmail.com>
2024-07-22 09:43:44 +0700

    Operator

---
+++ Operator.py
@@ -0,0 +1,25 @@
+# + - * / **
+print(2+3)
+print(2-3)
+print(4*2)
+print(10/2)
+print(2**3)
+# BMI program
+weight = input()
+height = input()
+print("BMI = " + str(float(weight)/float(height)**2))
+# Round function
+print(round(3.14159, 2))
+print(round(2.5))
+# +=, -=, /=, *=
+num = 0
+num += 2
+print(num)
+num -= 1
+print(num)
+num *= 10
+print(num)
+num **= 10
+print(num)
+num /= 20
+print(num)
