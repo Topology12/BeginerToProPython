c776abab86661deb33dbd361af06d58459ac039f
Topology12 <duong120503mk@gmail.com>
2024-07-24 15:52:50 +0700

    Basic_8

---
+++ Basic_8.py
@@ -0,0 +1,17 @@
+# Function, while loop
+# Function
+def my_function():
+    print("I love Hoang Quynh")
+
+
+my_function()
+
+# While loop
+target = 100
+curr = 0
+
+while curr < 100:
+    curr += 1
+
+if curr == 100:
+    print("Got target")
